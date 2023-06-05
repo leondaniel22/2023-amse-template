b2856468ddd97b79111445fe546054111967bcf1
leondaniel22 <leondaniel22@web.de>
2023-06-05 18:26:57 +0200

    added first version of exercise2.jv

--- project/test.sh
+++ project/test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh  
 
 # execute the pipeline
-echo "Execute the pipeline"
+echo "Execute the pipeline ..."
 python data/pipeline_script.py
 
 # test if pipeline works correct
-echo "Test if pipeline works correctly"
+echo "Test if pipeline works correctly ..."
 pytest data/test_pipeline.py
 
