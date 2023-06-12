8d388b39e0d020bee6b5b208a6ec524b6fdd3c93
leondaniel22 <leondaniel22@web.de>
2023-06-12 22:03:46 +0200

    testing 2.0

--- project/test.sh
+++ project/test.sh
@@ -1,12 +1,14 @@
 #!/bin/sh  
 
-pip install -r .testing/requirements.txt
+cd testing
+
+pip install -r requirements.txt
 
 # execute the pipeline
 echo "Execute the pipeline ..."
-python ./testing/data/pipeline_script.py
+python ./data/pipeline_script.py
 
 # test if pipeline works correct
 echo "Test if pipeline works correctly ..."
-pytest ./testing/data/test_pipeline.py
+pytest ./data/test_pipeline.py
 
